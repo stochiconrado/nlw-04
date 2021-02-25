093016920a51a7f37f6eb87fcabd0e6e66746714
Conrado Stochi <stochiconrado@gmail.com>
2021-02-25 12:45:56 -0300

    Day#04. Add notifications on browser. Finished app.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile(){
+  const {level} = useContext(ChallengesContext);
+
   return(
     <div className={styles.profileContainer}>
       <img src="https://github.com/stochiconrado.png" alt="Conrado Stochi"/>
@@ -8,7 +12,7 @@ export function Profile(){
         <strong>Conrado Stochi</strong>
         <p>
           <img src="icons/level.svg" alt=""/>
-          Level 1
+            Level {level}
         </p>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 import styles from "../styles/pages/Home.module.css";
 
 import { ExperienceBar } from "../components/ExperienceBar";
@@ -6,7 +6,7 @@ import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ChallengeBox } from "../components/ChallengeBox";
-
+import { CountdownProvider } from "../contexts/CountdownContext";
 
 export default function Home() {
   return (
@@ -14,19 +14,19 @@ export default function Home() {
       <Head>
         <title>Início | move.it</title>
       </Head>
-      <ExperienceBar/>
-    <section>
-      <div> 
-        <Profile/>
-        <CompletedChallenges/>
-        <Countdown/>
-      </div>
-      <div>
-        <ChallengeBox/>
-      </div>
-    </section>
-    
-    
+      <ExperienceBar />
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
-  )
+  );
 }
